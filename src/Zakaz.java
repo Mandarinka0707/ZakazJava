604f8d4a7234625d6d785ec6cc97317807f052dc
Mandarinka0707 <167908178+Mandarinka0707@users.noreply.github.com>
2025-01-04 08:05:52 -0800

    Update Zakaz.java

--- src/Zakaz.java
+++ src/Zakaz.java
@@ -3,37 +3,61 @@ import java.util.List;
 
 public class Zakaz  {
     private String opisanie;
-    private Status status = new Oformlen();
+    private StatusZakaza status;
     private int cost;
-    private List<String> cities = new ArrayList<>();
-     public Zakaz(String opisanie, int cost){
+    private String location;
+    public Zakaz(String opisanie,String location, int cost){
          this.cost=cost;
          this.opisanie=opisanie;
-     }
-     public void addCities(String city){
-         if(city!=null){
-             cities.add(city);
-         }
-     }
+         this.location=location;
+    }
+    public void setStatus(){
+        if((cost>0) && (status==StatusZakaza.REGISTERED)){
+            status=StatusZakaza.PAID;
+            System.out.println(status.getTitle());
+        }
+        else if(status==StatusZakaza.PAID ){
+            status=StatusZakaza.TRANSIT;
+            System.out.println(status.getTitle() +" "+getLocation());
+        }
+        else if(status==StatusZakaza.TRANSIT && location!=null){
+            status=StatusZakaza.TRANSIT;
+            System.out.println(status.getTitle() +" "+getLocation());
+        }
+        else if(status==StatusZakaza.TRANSIT && location==null){
+            status=StatusZakaza.DELVERED;
+            System.out.println(status.getTitle());
+        }
+        else if(status!=StatusZakaza.TRANSIT && status!=StatusZakaza.PAID && status!=StatusZakaza.DELVERED ){
+            status=StatusZakaza.REGISTERED;
+            System.out.println(status.getTitle());
+        }
+        else{
+            System.out.println("end");
+        }
 
-    public List<String> getCities() {
-        return cities;
+    }
+    public String getLocation() {
+        if(status==StatusZakaza.TRANSIT && location!=null){
+            return this.location;
+        }
+        else{
+            return "Заказ не в доставке";
+        }
     }
 
-    public void setStatus(Status state){
-          this.status=state;
-     }
-     public void status(){
-          status.status(this);
-     }
+    public void setLocation(String location) {
+        if(status==StatusZakaza.TRANSIT) {
+            this.location = location;
+        }
+    }
 
     public int getCost() {
         return cost;
     }
-    public Status getStatus() {
-        return status;
+    public String getStatus() {
+        return status.getTitle();
     }
-
     public String getOpisanie() {
         return opisanie;
     }
