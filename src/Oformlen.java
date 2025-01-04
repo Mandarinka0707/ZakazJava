dcbf6dbb5eef796570443554486058495d6753aa
Mandarinka0707 <167908178+Mandarinka0707@users.noreply.github.com>
2025-01-04 08:07:53 -0800

    Delete src/Oformlen.java

--- src/Oformlen.java
+++
@@ -1,7 +0,0 @@
-public class Oformlen implements Status{
-    @Override
-    public void status(Zakaz zakaz) {
-        System.out.println(zakaz.getOpisanie()+"  "+ "Оформлен");
-        zakaz.setStatus(new Oplachen());
-    }
-}
