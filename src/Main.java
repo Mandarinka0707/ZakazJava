cf476cbdbca6d9770ee12525896d657b553f7f05
Mandarinka0707 <167908178+Mandarinka0707@users.noreply.github.com>
2025-01-04 08:08:23 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Zakaz z1 = new Zakaz("Телефон",4000);
-        z1.addCities("Москва");
-        z1.addCities("Cаратов");
-        z1.addCities("Казань");
+        Zakaz z1 = new Zakaz("Телефон","Москва",4000);
+        z1.setStatus();
+        z1.setStatus();
+        z1.setStatus();
+        z1.setLocation("Саратов");
+        z1.setStatus();
+        z1.setLocation(null);
+        z1.setStatus();
 
-        z1.status();
-        z1.status();
-        z1.status();
-        z1.status();
-        z1.status();
-        z1.status();
+
 
 
 
