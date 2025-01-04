198feacf6dac3b53c004ce8467915dea4d52bdef
Mandarinka0707 <167908178+Mandarinka0707@users.noreply.github.com>
2025-01-04 08:29:39 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import Zakaz.*;
 public class Main {
     public static void main(String[] args) {
         Zakaz z1 = new Zakaz("Телефон","Москва",4000);
         z1.setStatus();
+        System.out.println(z1.getStatus());
         z1.setStatus();
+        System.out.println(z1.getStatus()+z1.getLocation());
         z1.setStatus();
+        System.out.println(z1.getStatus()+z1.getLocation());
         z1.setLocation("Саратов");
         z1.setStatus();
+        System.out.println(z1.getStatus()+z1.getLocation());
+        System.out.println(z1);
         z1.setLocation(null);
         z1.setStatus();
+        System.out.println(" ---------------");
+        z1.getStatus();
+
+
 
 
 
