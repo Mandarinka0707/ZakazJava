08a7a5df503f6dbef6ceb0dc0e339c157c76edf9
Mandarinka0707 <167908178+Mandarinka0707@users.noreply.github.com>
2025-01-04 08:07:41 -0800

    Delete src/Oplachen.java

--- src/Oplachen.java
+++
@@ -1,9 +0,0 @@
-public class Oplachen implements Status {
-    @Override
-    public void status(Zakaz zakaz) {
-        if(zakaz.getCost()>=0){
-            System.out.println(zakaz.getOpisanie()+" "+"Цена "+zakaz.getCost()+" "+"Оплачен");
-        }
-        zakaz.setStatus(new Vdostavke());
-    }
-}
